8a4dcf99d4e3f86727101690862783cb416c2cc9
Teguh <teguh@192.168.1.15>
2025-10-29 17:11:54 +0700

    list item active

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import ListGroup from "./components/ListGroup";
 
+ let items = [
+    'Palembang',
+    'Lampung',
+    'Madiun',
+    'Bali',
+    'Jakarta'];
+
 function App() {
-  return <div><ListGroup /></div>; 
+  return <div><ListGroup items={items} heading="Cities"/></div>; 
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 
-function ListGroup(){
-  let items = [
-    'Palembang',
-    'Lampung',
-    'Madiun',
-    'Bali',
-    'Jakarta'];
+
+interface Props {
+  items: string [];
+  heading: string;
+}
+
+function ListGroup({items, heading}: Props){
+ 
 
     const [selectedIndex, setSelectedIndex] = useState (-1);
 
     return(
       <>
-        <h1>List</h1>
+        <h1>{heading}</h1>
         {items.length === 0 && <p>No items found</p>}
             <ul className="list-group">
               {items.map((item, index) => (
