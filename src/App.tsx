b2e4a5485fe5e6791c9615df4cbc146991c1b6c2
Teguh <teguh@192.168.1.7>
2025-11-03 11:24:45 +0700

    Button click and color

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Button from "./components/Button";
 function App() {
   return (
     <div>
-      <Button color="info"  onClick={() => console.log('Clicked')}>My Button</Button>
+      <Button color="dark"  onClick={() => console.log('Clicked')}>My Button</Button>
     </div>
   );
 }
