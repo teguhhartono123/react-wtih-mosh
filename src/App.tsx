f7060e1c87254c96a7798a23c8d8887f559206a0
Teguh <teguh@Teguhs-MacBook-Pro.local>
2025-11-05 14:34:00 +0700

    css in js

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
-import Alert from "./components/Alert";
-import Button from "./components/Button";
+// import { useState } from "react";
+// import Alert from "./components/Alert";
+// import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+import "./App.css";
 
 function App() {
-  const[alertVisible, setAlertVisibility] = useState(false);
-
+  const items = ["Palembang", "Lampung", "Jakarta", "Ponorogo", "Bali","Bandung", "Surabaya", "Medan"];
 
   return (
     <div>
-      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>}
-      <Button color="info "  onClick={() => setAlertVisibility(true)}>My Button</Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectitem={(item) => alert(`You selected ${item}`)}
+      />
     </div>
   );
 }
--- src/components/ListGroup/ListGroup.tsx
+++ src/components/ListGroup/ListGroup.tsx
@@ -1,5 +1,21 @@
 import { useState } from "react";
+import "./ListGroup.css";
+import styled from 'styled-components';
 
+
+const List = styled.ul`  
+  list-style: none;
+  padding: 0;
+`
+
+interface ListItemProps {
+  active: boolean;
+}
+
+const ListItem = styled.li<ListItemProps>`
+padding: 5px 0;
+background: ${(props) => (props.active ? "lightblue" : "none")};
+`
 
 interface Props {
   items: string [];
@@ -8,28 +24,26 @@ interface Props {
 }
 
 function ListGroup({items, heading, onSelectitem}: Props){
- 
-
-    const [selectedIndex, setSelectedIndex] = useState (-1);
+const [selectedIndex, setSelectedIndex] = useState (0);
 
     return(
       <>
         <h1>{heading}</h1>
         {items.length === 0 && <p>No items found</p>}
-            <ul className="list-group">
+            <List>
               {items.map((item, index) => (
-                <li 
-                  className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item' }
+                <ListItem
+                  active={index === selectedIndex}
                   key={item} 
-                    onClick={() => 
+                  onClick={() => 
                       {setSelectedIndex(index); 
                       onSelectitem(item);
                       }}
                   >
                    {item} 
-                </li>
+                </ListItem>
               ))}
-            </ul>
+            </List>
       </>
     )
 }
