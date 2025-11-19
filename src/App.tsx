9c1fc02cedfac17e7855c11708cbc4509a7b312d
Teguh <teguh@192.168.1.15>
2025-11-19 11:40:43 +0700

    sharing state between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,16 @@
-
-//import Button from "./components/Button/Button.module.css";
-import ListGroup from "./components/ListGroup";
-import "./App.css";
-import Message from "./Message";
-
-
-import { CgAdidas, CgChanel, CgAppleWatch } from "react-icons/cg";
-import Like from "./components/Like";
-import { useState } from "react";
-
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import Cart from './components/Cart'
 
 function App() {
-  const items = ["Palembang", "Lampung", "Jakarta", "Ponorogo", "Bali","Bandung", "Surabaya", "Medan"];
-  
-  const [drink, setDrink] =  useState({
-    title: 'Americano',
-    price: 5,
-  });
-  const handleClick = () => {
-    setDrink( {...drink, price : 7} );
-  };
-
+  const [cartItems, setCartItems] = useState(['Product1', 'Product2'])
 
   return (
     <div>
-
-      {drink.price}
-      <button onClick={handleClick}> Click me </button>
-
-      <br/><br/>
-      <Message  />
-      <Message  />
-      <Message  />
-   
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectitem={(item) => alert(`You selected ${item}`)}
-      />
-
-      <CgAdidas size={150} color="Blue" />
-      <CgChanel size={150} color="Dark" />
-      <CgAppleWatch size={150} color="Silver" />
-      
-      <button onClick={() => {} }>My Button </button>
-      <br/><br/>
-
-
-      <Like onClick={() => console.log('clicked')}/>
+      <NavBar cartItemsCount={cartItems.length} />
+      <Cart cartItems={cartItems} onClear={() => setCartItems([])}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
