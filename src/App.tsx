ae510d25cbfc543a41aa221911b520e90f7a3812
Teguh <teguh@Teguhs-MacBook-Pro.local>
2025-11-24 17:21:07 +0700

    create form and hanling form submission

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import ExpandableText from './components/ExpandableText'
+import Form from './components/Form';
 
-function App() {
- 
+const App = () => {
   return (
     <div>
-    <ExpandableText> 
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis iusto tempore ratione. Commodi, numquam obcaecati! Nihil deserunt dolorem enim! Saepe dicta esse repellat, a odit distinctio maiores non ab quia itaque ex, ad impedit repellendus exercitationem quidem obcaecati in error blanditiis iure. Quidem, dolor quia ab magni doloremque quod optio aliquam ea repellendus eveniet! Voluptas cupiditate unde ipsum error aliquid minus delectus quasi quas nostrum assumenda odio ad, commodi dolore repudiandae illum ab consectetur maxime placeat sed accusantium, voluptatibus aliquam est in dicta! Necessitatibus quis autem dolorum cupiditate deleniti cumque, corporis explicabo laudantium esse delectus ipsam perferendis ducimus laboriosam illum?
-    </ExpandableText>
+      <Form />
     </div>
   )
 }
