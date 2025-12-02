663f20ae7bf4a9c43108537863f544bbc3d28f3e
Teguh <teguh@Teguhs-MacBook-Pro.local>
2025-12-02 11:52:49 +0700

    integrate react hook & zood for validation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList"
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App(){
   const [selectedCategory, setSelectedCategory] = useState('');
@@ -24,6 +26,9 @@ function App(){
 
   return (
     <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
       <div className="mb-3">
         <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)}/>
       </div>
--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import categories from '../categories';
 
 interface Props{
     onSelectCategory: (category: string) => void;
@@ -8,9 +9,7 @@ const ExpenseFilter = ({onSelectCategory} : Props) => {
   return (
     <select className="form-select" onChange={(event) => onSelectCategory(event.target.value)}>
         <option value="">All categories</option>
-        <option value="Groceries">Groceries</option>
-        <option value="Utilities">Utilities</option>
-        <option value="Entertainment">Entertainment</option>
+        {categories.map(category => <option key={category} value={category}>{category}</option>)}
     </select>
 )}
 
