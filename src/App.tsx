cfaa10a56ef08217b3853c7b68f83533a644a79e
Teguh <teguh@Teguhs-MacBook-Pro.local>
2025-12-04 18:52:36 +0700

    handling error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
-
 interface User{
   id: number;
   name: string;
@@ -9,17 +8,23 @@ interface User{
 
 function App () {
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
+
 
   useEffect (() => {
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/xusers')
       .then(res => setUsers(res.data))
+      .catch(err => setError(err.message))
   }, [])
 
 
   return (
+    <>
+    {error && <p className="text-danger">{error}</p>}
     <ul>
       {users.map(user => <li key={user.id}>{user.name}</li>)}
     </ul>
+    </>
   )
 }
 
