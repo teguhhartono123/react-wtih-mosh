3feae596de4d62d0eb6713ed26a8e00de2f4412d
Teguh <teguh@Teguhs-MacBook-Pro.local>
2025-12-04 17:00:36 +0700

    Effect dependencies - Product List case

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,22 @@
+import React, { useEffect, useState } from 'react'
+import ProductList from './components/ProductList'
 
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList"
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+function App() {
 
-function App(){
-  const [selectedCategory, setSelectedCategory] = useState('');
+const [category, setCategory]= useState('');
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: 'aaa', amount: 10, category: 'Utilities'},
-    { id: 2, description: 'bbb', amount: 10, category: 'Utilities'},
-    { id: 3, description: 'ccc', amount: 10, category: 'Utilities'},
-    { id: 4, description: 'ddd', amount: 10, category: 'Utilities'},
-    { id: 5, description: 'eee', amount: 10, category: 'Groceries'},
-    { id: 6, description: 'fff', amount: 10, category: 'Groceries'},
-    { id: 7, description: 'ggg', amount: 10, category: 'Groceries'},
-    { id: 8, description: 'hhh', amount: 10, category: 'Entertainment'},
-
-  ])
-
-  const visibleExpenses = selectedCategory 
-    ? expenses.filter((e) => e.category === selectedCategory) 
-    : expenses;
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm onSubmit={expense => setExpenses([...expenses, {...expense, id: expenses.length + 1 }])}/>
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter onSelectCategory={(category) => setSelectedCategory(category)}/>
-      </div>
-      <ExpenseList 
-        expenses={visibleExpenses} 
-        onDelete={(id) => setExpenses(expenses.filter(e => e.id!== id ))} />
+      <select className="form-select" onChange={(event) => setCategory(event.target.value)}>
+        <option value=""></option>
+        <option value="Clothing">Clothing</option>
+        <option value="Household">Household</option>
+        <option value="Gardening">Gardening</option>
+      </select>
+      <ProductList category={category}/>
     </div>
-  );
+  )
 }
 
 export default App
