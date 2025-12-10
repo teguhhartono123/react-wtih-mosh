84d5d2d04ba1ee097493feb36182918f937a98b8
Teguh <teguh@Teguhs-MacBook-Pro.local>
2025-12-10 17:34:51 +0700

    Update Data Patch Saved in server temp

--- src/App.tsx
+++ src/App.tsx
@@ -33,40 +33,69 @@ function App() {
     return () => controller.abort();
   }, []);
 
-  const deleteUser = (user: User) =>{
+  const deleteUser = (user: User) => {
     const originalUsers = [...users];
-    setUsers(users.filter(u => u.id !== user.id));
+    setUsers(users.filter((u) => u.id !== user.id));
 
-    axios.delete('https://jsonplaceholder.typicode.com/xusers/' + user.id)
-      .catch(err => {
+    axios
+      .delete("https://jsonplaceholder.typicode.com/xusers/" + user.id)
+      .catch((err) => {
         setError(err.message);
         setUsers(originalUsers);
-      })
-  }
-  
+      });
+  };
+
   const addUser = () => {
     const originalUsers = [...users];
-    const newUser = { id:0, name: 'Teguh'};
-    setUsers ([newUser, ...users])
+    const newUser = { id: 0, name: "Teguh" };
+    setUsers([newUser, ...users]);
 
-    axios.post('https://jsonplaceholder.typicode.com/users', newUser)
-      .then(({ data : savedUser}) => setUsers([savedUser, ...users]))
-      .catch(err => {
-        setError(err.message)
-        setUsers(originalUsers)
+    axios
+      .post("https://jsonplaceholder.typicode.com/users", newUser)
+      .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
       });
+  };
+
+  const updateUser = (user: User) =>{
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + '!'};
+    setUsers (users.map(u => u.id === user.id ? updatedUser : u))
+
+    axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser)
+    .catch(err => {
+      setError(err.message);
+      setUsers(originalUsers);
+    })
   }
+
+
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <button className="btn btn-primary mb-3" onClick={addUser}>Add</button>
+      <button className="btn btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
       <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id} className='list-group-item d-flex justify-content-between'>
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
             {user.name}{" "}
-            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            <div>
+              <button className="btn btn-outline-secondary mx-1" onClick={() => updateUser(user)}>Update</button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
+            </div>
           </li>
         ))}
       </ul>
