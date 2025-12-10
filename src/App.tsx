22c24095240b1b6498061bcf265e8d8683d054b1
Teguh <teguh@192.168.1.6>
2025-12-10 21:17:34 +0700

    Extracting a Reusable API Client for Backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios, { CanceledError } from "axios";
-import { tr } from "zod/locales";
+import apiClient, { CanceledError } from "./services/api-client";
 
 interface User {
   id: number;
@@ -16,8 +15,8 @@ function App() {
     const controller = new AbortController();
 
     setLoading(true);
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users", {
+    apiClient
+      .get<User[]>("/users", {
         signal: controller.signal,
       })
       .then((res) => {
@@ -37,8 +36,8 @@ function App() {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
 
-    axios
-      .delete("https://jsonplaceholder.typicode.com/xusers/" + user.id)
+    apiClient
+      .delete("/users/" + user.id)
       .catch((err) => {
         setError(err.message);
         setUsers(originalUsers);
@@ -50,8 +49,8 @@ function App() {
     const newUser = { id: 0, name: "Teguh" };
     setUsers([newUser, ...users]);
 
-    axios
-      .post("https://jsonplaceholder.typicode.com/users", newUser)
+    apiClient
+      .post("/users", newUser)
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
       .catch((err) => {
         setError(err.message);
@@ -64,13 +63,12 @@ function App() {
     const updatedUser = { ...user, name: user.name + '!'};
     setUsers (users.map(u => u.id === user.id ? updatedUser : u))
 
-    axios.patch('https://jsonplaceholder.typicode.com/users/' + user.id, updatedUser)
+    apiClient.patch('/users/' + user.id, updatedUser)
     .catch(err => {
       setError(err.message);
       setUsers(originalUsers);
     })
   }
-
 
 
   return (
